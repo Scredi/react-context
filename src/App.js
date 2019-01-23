1041db5b858e82f6b684f85356f7d535d06cbfc3
Maxime Petronio <m.petronio91@gmail.com>
2019-01-23 12:33:33 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { TodoConsumer } from './store/TodoContext'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App">        
+        <TodoConsumer>          
+          {({ todos, deleteTodo, createTodo, todo, handleInput, validateTodo, handleKeyPress }) => (
+            <div>
+              <input onKeyPress={handleKeyPress} type='text' value={todo} onChange={handleInput}/>
+              <button onClick={createTodo}>Ajouter</button>
+              {
+                todos.map((item, index) => {
+                  let isComplete = item.complete ? 'line-through' : 'none'
+                  return (
+                    <div key={index}>
+                      <p style={{textDecoration: isComplete}}>{item.todo}</p>
+                      <button onClick={deleteTodo.bind(this, index)}>Supprimer</button>
+                      <button onClick={validateTodo.bind(this, index)}>Valider</button>
+                    </div>
+                  )
+                })
+              }
+            </div>
+          )}
+        </TodoConsumer>
       </div>
     );
   }
---
+++ src/store/TodoContext.js
@@ -0,0 +1,79 @@
+import React, { Component, createContext } from 'react'
+
+const TodoContext = createContext()
+
+class TodoProvider extends Component {
+    
+    constructor() {
+        super()
+
+        this.state = {
+            todos: [
+                {
+                    todo: 'Hello',
+                    complete: false
+                },
+                {
+                    todo: 'World',
+                    complete: false 
+                }
+            ],
+            todo: ''            
+        }
+    }
+
+    createTodo = () => {        
+        let currentTodo = { todo: this.state.todo, complete: false }
+        let todos = [...this.state.todos, currentTodo]        
+        this.setState({
+            todos: todos,
+            todo: ''
+        })
+    }
+
+    validateTodo = indexToComplete => {
+        let todos = this.state.todos
+        todos[indexToComplete].complete = !todos[indexToComplete].complete
+        this.setState({
+            todos: todos
+        })
+    }
+
+    deleteTodo = indexToDelete => {
+        this.setState({
+            todos: this.state.todos.filter((todo, index) => index !== indexToDelete)
+        })
+    }
+
+    handleKeyPress = e => {
+        return e.key === 'Enter' && this.createTodo()
+    }
+
+    handleInput = e => {
+        this.setState({
+            todo: e.target.value
+        })
+    }
+
+    render() {
+        return(
+            <TodoContext.Provider
+                value={{ 
+                    todos: this.state.todos,
+                    deleteTodo: this.deleteTodo,
+                    createTodo: this.createTodo,
+                    handleInput: this.handleInput,
+                    todo: this.state.todo,
+                    validateTodo: this.validateTodo,
+                    handleKeyPress: this.handleKeyPress
+                }}
+            >
+            {this.props.children}
+            </TodoContext.Provider>
+        )
+    }
+}
+
+const TodoConsumer = TodoContext.Consumer
+
+export { TodoProvider, TodoConsumer }
